e77a0577efb9410f5141771c65a4bb78adc67275
Corey Munn <michael.munn@secunetics.com>
2024-07-01 20:40:31 -0400

    Create Navbar with Auth and install in the Layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 border-b border-slate-200 backdrop-blur-md transition-all">
+      <MaxWidthWrapper className="">
+        <div className="flex justify-between items-center py-2">
+          <Link href="/" className="font-semibold text-primary">
+            PDF.ly
+          </Link>
+          <div className="items-center space-x-4 sm:flex">
+            <Link
+              href="/pricing"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              Pricing
+            </Link>
+
+            <LoginLink
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              Sign In
+            </LoginLink>
+            <RegisterLink
+              className={buttonVariants({
+                size: "sm",
+              })}
+            >
+              Get Started
+            </RegisterLink>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
