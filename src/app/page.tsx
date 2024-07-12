cf502a26c25cacaf5588cedd490e3a1aeb2ec95a
Corey Munn <michael.munn@secunetics.com>
2024-07-12 11:27:19 -0400

    remove async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 
-const Home = async () => {
+const Home = () => {
   return (
     <>
       <LandingPageSection>
