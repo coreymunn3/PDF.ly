f7c13b2f4865431a7f6d8278f009e41fe85f8950
Corey Munn <michael.munn@secunetics.com>
2024-07-18 20:27:19 -0400

    scaffold out the file page with PDF renderer and chat wrapper. Install react-pdf for later use

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
 import db from "@/db";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
+import ChatWrapper from "@/components/ChatWrapper";
 
 interface PageProps {
   params: {
@@ -28,7 +31,25 @@ const Page = async ({ params }: PageProps) => {
   console.log(file);
 
   // render the file
-  return <div>page</div>;
+  return (
+    <MaxWidthWrapper>
+      <div className="flex flex-col flex-1 justify-between h-[calc(100vh - 3.5rem)]">
+        <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+          {/* PDF Left side */}
+          <div className="flex-1 xl:flex">
+            <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1">
+              <PdfRenderer />
+            </div>
+          </div>
+
+          {/* Chat Right Side */}
+          <div className="shrink-0 flex-[0.75] border-t border-slate-200 lg:w-96 lg:border-l lg:border-t-0">
+            <ChatWrapper />
+          </div>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
 };
 
 export default Page;
---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,11 @@
+const PdfRenderer = () => {
+  return (
+    <div className="w-full bg-white rounded-md flex flex-col items-center">
+      <div className="h-14 w-full border-b border-slate-200 items-center justify-between px-2">
+        <div className="flex h-full items-center gap-2">control bar</div>
+      </div>
+    </div>
+  );
+};
+
+export default PdfRenderer;
